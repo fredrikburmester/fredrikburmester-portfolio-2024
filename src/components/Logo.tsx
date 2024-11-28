cb07c6a03543c0b6dd777c8a1559c0e8ef029d9e
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-11-28 14:27:56 +0100

    fix: add profile picture

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+
 export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return <p>fredrik burmester</p>
+  return (
+    <div className="flex aspect-square w-48 items-center justify-center overflow-hidden rounded-full">
+      <Image
+        src="/images/pp2.jpg"
+        alt="Logo"
+        className="w-full object-cover"
+        width={100}
+        height={100}
+      />
+    </div>
+  )
 }
